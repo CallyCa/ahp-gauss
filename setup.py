c4bd4f5927febd7054d0353509ec307a80c2adc0
callyca <blackyrite@hotmail.com>
2024-11-13 11:01:52 -0300

    gaussian ahp

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r", encoding="utf-8") as fh:
 
 setup(
     name="ahp-gauss",
-    version="0.1.2",
+    version="0.1.3",
     description="The AHP-Gaussian Decision Support Library is a extensible solution designed for multi-criteria decision-making (MCDM) leveraging both traditional and advanced AHP methodologies",
     long_description=long_description,
     long_description_content_type="text/markdown",
