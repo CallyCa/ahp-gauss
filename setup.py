32d9b91203742da797b51970a8e9644bf4c6956b
callyca <blackyrite@hotmail.com>
2024-11-13 10:31:16 -0300

    gaussian ahp

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup, find_packages
 setup(
     name="decision_support_system",
     version="0.1.0",
-    packages=find_packages(where="src"),
+    packages=find_packages(),
     package_dir={"": "src"},
     install_requires=[
         "pandas",
