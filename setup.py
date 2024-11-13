f03e278607c31cbec6d2c29adb9d68eca50c875c
callyca <blackyrite@hotmail.com>
2024-11-13 10:59:07 -0300

    gaussian ahp

--- setup.py
+++ setup.py
@@ -1,8 +1,14 @@
 from setuptools import setup, find_packages
 
+with open("README.md", "r", encoding="utf-8") as fh:
+    long_description = fh.read()
+
 setup(
     name="ahp-gauss",
     version="0.1.1",
+    description="Descrição curta do projeto",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
     packages=find_packages(),
     install_requires=[
         "pandas",
